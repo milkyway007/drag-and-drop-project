fc56f49d9e434632dbc75e5d102acd1e77793c87
Liidia Laada <liidia@sixfold.com>
2022-07-27 11:32:00 +0300

    Use ES modules, understand various import and export syntaxes

--- src/decorators/autobind.ts
+++ src/decorators/autobind.ts
@@ -1,14 +1,12 @@
-namespace App {
-  // Autobind decorator
-  export function autobind(_: any, __: string, descriptor: PropertyDescriptor) {
-    const originalMethod = descriptor.value;
-    const adjDescriptor = {
-      configurable: true,
-      get() {
-        const boundFn = originalMethod.bind(this);
-        return boundFn;
-      },
-    };
-    return adjDescriptor;
-  }
+// Autobind decorator
+export function autobind(_: any, __: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
 }
